85869344bdf35893fe2d6003b1e62158388b30c8
Dmitry <99917230+dnp34@users.noreply.github.com>
2023-04-30 11:10:45 +0300

    Create dataURLtoFile.js

---
+++ js/dataURLtoFile.js
@@ -0,0 +1,12 @@
+function dataURLtoFile(dataurl, filename) {
+    let arr = dataurl.split(','),
+        mime = arr[0].match(/:(.*?);/)[1],
+        bstr = atob(arr[1]),
+        n = bstr.length,
+        u8arr = new Uint8Array(n);
+
+    for (let i = 0; i < n; i++) {
+        u8arr[i] = bstr.charCodeAt(i);
+    }
+    return new File([u8arr], filename, { type: mime });
+}
